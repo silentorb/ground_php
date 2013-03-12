04fd6ad6342c47846504bdae7bbaf5527c70e3de
silentorb <christopherwjohnson@gmail.com>
2013-03-12 13:28:14 -0600

    Added inserting new embedded reference objects.

--- test/tests/Update_Test.php
+++ test/tests/Update_Test.php
@@ -44,10 +44,31 @@ class Update_Test extends Ground_Test_Case {
     $objects = $this->ground->create_query('character_item')->run();
     $this->assertEquals('laser', $objects[2]->name);
     $this->assertEquals(2, $objects[2]->owner->id);
-    
-    $objects = $this->ground->create_query('warrior')->run();    
+
+    $objects = $this->ground->create_query('warrior')->run();
     $this->assertGreaterThan(1, count($objects));
     $this->assertEquals('object', gettype($objects[1]->inventory[0]));
+  }
+
+  function test_insert_new_embedded_reference() {
+    $this->fixture->load_schemas();
+    $this->fixture->prepare_database();
+
+    // Insert Embedded Object
+    $this->cyborg = $this->fixture->insert_object('character_item', array(
+        'name' => 'laser',
+        'owner' => array(
+            'name' => 'Cirguit',
+            'race' => 'cyborg',
+            'age' => 1,
+        )
+            ));
+
+    $objects = $this->ground->create_query('warrior')->run();
+    $this->assertEquals(1, $objects[0]->inventory[0]->id);
+
+    $objects = $this->ground->create_query('character_item')->run();
+    $this->assertEquals(1, $objects[0]->owner->id);
   }
 
 }
