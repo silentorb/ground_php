8dc916637f54ff118c97094c6f09f79d19d9e885
silentorb <christopherwjohnson@gmail.com>
2013-03-20 16:02:00 -0600

    Fixed a bug with running irrigation channels.

--- test/tests/Irrigation_Test.php
+++ test/tests/Irrigation_Test.php
@@ -1,5 +1,12 @@
 <?php
 
+class Temp {
+  function nothing() {
+    $this->called = true;
+  }
+
+}
+
 class Irrigation_Test extends Ground_Test_Case {
   function test_get_path_array() {
     $result = Irrigation::get_path_array('home/item/action', 'home');
@@ -41,10 +48,20 @@ class Irrigation_Test extends Ground_Test_Case {
     $channel = $irrigation->find_channel('harvest');
     $this->assertEquals('object', gettype($channel));
     $this->assertEquals('harvest_food', $channel->method);
-    
+
     $this->assertNotNull($irrigation->find_channel('mutant/killer/squirrels'));
     $this->assertNull($irrigation->find_channel('mutant/squirrels'));
     $this->assertNull($irrigation->find_channel('mutant/killer'));
+  }
+
+  function test_service() {
+    $temp = new Temp();
+    $this->fixture->load_schemas();
+    $irrigation = $this->ground->irrigation = new Irrigation($this->ground);
+    $irrigation->add_channel('kill/%', $temp, 'nothing');
+    $request = new Bag('kill/time');
+    $this->ground->vineyard_service($request);
+    $this->assertTrue($temp->called);
   }
 
 }
